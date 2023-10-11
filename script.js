3ec6efc1e2f1714debceddd135077f9864a5f0c3
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-10-11 19:43:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //your JS code here. If required.
+const status = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
 enterBtn.addEventListener("click", function() {
     status.textContent = "Entered Metaverse";
     status.style.fontSize = "24px"; // You can adjust the font size as needed
