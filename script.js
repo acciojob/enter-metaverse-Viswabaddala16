e29544b090403477deecc4c10dda785cd5c5319e
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-10-21 09:23:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ const enterBtn = document.getElementById("enterBtn");
 
 enterBtn.addEventListener("click", function() {
     status.textContent = "Entered Metaverse";
-    status.style.fontSize = "24px"; // You can adjust the font size as needed
+    status.style.fontSize = "29px";
+	status.style.fontWeight = "550";
+	
+	// You can adjust the font size as needed
 });
